a2b95d817a0e76d755b3d1aab2d42cd1ef53c2f7
kamila-k <kamila.kralovcova@gmail.com>
2022-01-22 10:09:41 +0100

    1 file modified

---
+++ index2.js
@@ -0,0 +1,12 @@
+// Import stylesheets
+import './style.css';
+// Write Javascript code!
+
+colsole.log('aoj');
+
+
+
+
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
