db2931d69fa8513489e6150f5d8c420102490833
kamila-k <kamila.kralovcova@gmail.com>
2022-01-22 09:33:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-gi3kpu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-gi3kpu)
---
+++ index.js
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './style.css';
+import './220122_javascript';
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
